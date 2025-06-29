b2fb18fa8a92a053f3c58b5de10c68a7ac620a5e
Parth <notme123177@gmail.com>
2025-06-29 19:11:43 +0530

    End of Section 8 Angular Basic

--- skinet/client/src/app/app.ts
+++ skinet/client/src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./layout/header/header";
+import { HttpClient } from '@angular/common/http';
+import { Product, products } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Header], //RouterOutlet
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
+
 })
-export class App {
+export class AppComponent implements OnInit  {
+  baseurl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient)
   protected title = 'Skinet';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseurl + 'products').subscribe({
+      next: response => {
+              console.log('API Response:', response);
+              this.products = response.data;
+            },
+
+      error: error => console.error(error),
+      complete: () => console.log('Complete')
+    })
+  }
 }
